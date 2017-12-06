dc4f3f8ad259305720f0fa72320e8d6b218d9d66
YonePC <YonePC@YONE>
2017-12-06 06:41:01 +0000

    extracted duplicated behavior sum to common method and added conditional guards

--- DuplicatedCode/src/case3/Calculator.java
+++ DuplicatedCode/src/case3/Calculator.java
@@ -3,26 +3,28 @@ package case3;
 public class Calculator {
 
     private long result;
+    boolean squares;
 
     public Calculator() {
         result = getResult();
     }
 
     public long sum(int min, int max) {
-        iterateToSumInRange(min, max);
+        iterateToSumInRange(min, max, squares = false);
         return result;
     }
 
     public long sumOfSquares(int min, int max) {
-        long result = getResult();
-        for (int i = min ; i <= max ; i++)
-            result += i * i;
+        iterateToSumInRange(min, max, squares = true);
         return result;
     }
 
-    private void iterateToSumInRange(int min, int max) {
-        for (int i = min ; i <= max ; i++)
-            result += i;
+    private void iterateToSumInRange(int min, int max, boolean squares) {
+        result = getResult();
+        for (int i = min; i <= max; i++) {
+            if (squares) result += i * i;
+            if (!squares) result += i;
+        }
     }
 
     private long getResult() {
