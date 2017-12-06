692c5bde1ba12459cd8f7c7ee0d1cc94cd67dea0
YonePC <YonePC@YONE>
2017-12-06 06:27:55 +0000

    extracted variable initialization to constructor

--- DuplicatedCode/src/case3/Calculator.java
+++ DuplicatedCode/src/case3/Calculator.java
@@ -2,8 +2,13 @@ package case3;
 
 public class Calculator {
 
+    private long result;
+
+    public Calculator() {
+        result = getResult();
+    }
+
     public long sum(int min, int max) {
-        long result = getResult();
         for (int i = min ; i <= max ; i++)
             result += i;
         return result;
